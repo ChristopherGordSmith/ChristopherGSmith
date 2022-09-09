fe333c7ea7e2a936ccec1fdffa8db1d0faade9d8
ChristopherGSmith <christophergsmith3975@gmail.com>
2022-09-09 00:26:53 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import { RouterLink, RouterView } from "vue-router";
       <n-grid style="height: 50px; width: 100%" :cols="4">
         <n-gi>
           <n-avatar
-            src="src/assets/ResumeLogo.png"
+            src="src/assets/ResumeLogo.PNG"
             :size="40"
             style="background: #ffffff"
           />
