c30147d2263695ba2709f67ce370f39bb3289252
ChristopherGSmith <christophergsmith3975@gmail.com>
2022-09-08 21:30:05 -0500

    Start up
    Basic set up for now. Going to add in routing for different pages such as "projects" and "blogs"

---
+++ ChristopherGSmith/src/App.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <n-layout position="absolute" :native-scrollbar="false">
+    <n-layout-header mode="horizontal" style="background: #e4572e">
+      <n-grid style="height: 50px; width: 100%" :cols="4">
+        <n-gi>
+          <n-avatar
+            src="src/assets/ResumeLogo.png"
+            :size="40"
+            style="background: #ffffff"
+          />
+        </n-gi>
+        <n-gi :span="2.5" :offset="0.5">
+          <router-link class="topNavigation" to="/">Home</router-link>
+          <router-link class="topNavigation" to="/about">About me</router-link>
+        </n-gi>
+      </n-grid>
+    </n-layout-header>
+    <n-layout>
+      <n-space
+        justify="center"
+        align="center"
+        style="background: #011627; min-height: 750px"
+      >
+        <RouterView />
+      </n-space>
+    </n-layout>
+    <n-layout-footer>
+      <n-grid
+        style="background: #e07a5f; min-height: 100px; width: 100%"
+        :cols="3"
+        item-responsive
+        :x-gap="100"
+      >
+        <n-gi>
+          <n-space vertical>
+            <n-h3>item 1 </n-h3>
+            <n-h3>item 2</n-h3>
+          </n-space>
+        </n-gi>
+        <n-gi>
+          <n-space vertical>
+            <n-h3>item 1 </n-h3>
+            <n-h3>item 2</n-h3>
+          </n-space>
+        </n-gi>
+        <n-gi>
+          <n-space vertical>
+            <n-h3>item 1 </n-h3>
+            <n-h3>item 2</n-h3>
+          </n-space>
+        </n-gi>
+      </n-grid>
+    </n-layout-footer>
+  </n-layout>
+</template>
+
+<style scoped>
+</style>
---
+++ ChristopherGSmith/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <n-grid :cols="2">
+    <n-gi>
+      <n-space vertical align="center"
+        ><n-h1 style="color: white; font-weight: 400">Christopher Smith</n-h1>
+        <n-avatar round src="src/assets/ProfessionalPic.jpg" :size="400"
+      /></n-space>
+    </n-gi>
+    <n-gi>
+      <n-space vertical align="center" style="width: 600px"></n-space>
+      <n-h2 style="color: white"
+        >I am an undergradute student studying Computer Science at The
+        University of Texas at Arlington.</n-h2
+      >
+      <n-h2 style="color: white"
+        >I am currently employed at Ayoka Systems as an intern for Web
+        Application Development.</n-h2
+      >
+      <n-h2 style="color: white">
+        I am currently interested in AI and Web Application Development.
+      </n-h2>
+    </n-gi>
+  </n-grid>
+</template>
