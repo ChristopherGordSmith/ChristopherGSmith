8d74b854f649d6ce64938a79d7928d4b13ab6973
ChristopherGSmith <christophergsmith3975@gmail.com>
2022-09-09 08:23:59 -0500

    quick push

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,14 +6,15 @@
     <n-gi>
       <n-space vertical align="center"
         ><n-h1 style="color: white; font-weight: 400">Christopher Smith</n-h1>
-        <n-avatar
-          round
-          src="/./christophergsmith/src/assets/professionalpic.jpg"
-          :size="400"
+        <n-avatar round src="/src/assets/professionalpic.jpg" :size="400"
       /></n-space>
     </n-gi>
     <n-gi>
-      <n-space vertical align="center" style="width: 600px"></n-space>
+      <n-space
+        vertical
+        align="center"
+        style="width: 600px; height: 100%"
+      ></n-space>
       <n-h2 style="color: white"
         >I am an undergradute student studying Computer Science at The
         University of Texas at Arlington.</n-h2
