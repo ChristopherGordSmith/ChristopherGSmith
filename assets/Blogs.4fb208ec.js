c001e22f756e9b6114af729ee4889fc652b10bd3
ChristopherGSmith <christophergsmith3975@gmail.com>
2022-10-01 01:18:59 -0500

    working on a new commit

---
+++ assets/Blogs.4fb208ec.js
@@ -0,0 +1,1 @@
+import{_ as e,o,e as t,f as s}from"./index.9c8318a6.js";const c={},n=s("h1",{style:{color:"white"}},"No blogs have been uploaded yet",-1),a=[n];function r(_,l){return o(),t("div",null,a)}const f=e(c,[["render",r]]);export{f as default};
