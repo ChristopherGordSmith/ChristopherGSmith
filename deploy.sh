86042bc44be2f49d214eb9784ca47b7abc638b86
ChristopherGSmith <christophergsmith3975@gmail.com>
2022-10-05 23:34:50 -0500

    Attempting Vite fix

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+set -e
+
+npm run build
+
+cd dist
+
+echo > .nojekyll
+
+git init
+git checkout -b main
+git add -A 
+git commit -m 'deploy'
+git push -f git@github.com:ChristopherGordSmith/ChristopherGSmith.git main:gh-pages
